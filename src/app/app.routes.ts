4a27d6641c277df64916a1426bd2adf42901f802
Laura Moreno <chipifsdev@gmail.com>
2025-08-11 22:37:14 +0200

    añadidas las rutas de las paginas principales // modificado el config.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Rutas } from './pages/rutas/rutas';
+import { Tienda } from './pages/tienda/tienda';
+import { Contacto } from './pages/contacto/contacto';
+import { Alfareros } from './pages/alfareros/alfareros';
+import { Eventos } from './pages/eventos/eventos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', component: Home },
+    { path: 'rutas', component: Rutas },
+    { path: 'tienda', component: Tienda },
+    { path: 'eventos', component: Eventos },
+    { path: 'alfareros', component: Alfareros },
+    { path: 'contacto', component: Contacto }
+];
