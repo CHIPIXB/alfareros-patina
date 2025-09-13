33c7d83ddb8c5ec443218f41087f992e9623d66f
Laura Moreno <chipifsdev@gmail.com>
2025-09-13 18:05:36 +0200

    añadido el router outlet a la pagina de rutas para las rutas hijas

--- src/app/pages/rutas/rutas.ts
+++ src/app/pages/rutas/rutas.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-rutas',
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './rutas.html',
   styleUrl: './rutas.css'
 })
