ae192a98900710ef86cd65fe5ceb0ff2185aa70b
Laura Moreno <chipifsdev@gmail.com>
2025-09-22 12:42:22 +0200

    añadida a wikiloc la ruta alfarera a fuego, añadido el carrusel en la seccion de mapa de wikiloc con 4 imagenes, 1 de cada tipo de ruta

--- src/app/pages/wikiloc-rutas/wikiloc-rutas.ts
+++ src/app/pages/wikiloc-rutas/wikiloc-rutas.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-wikiloc-rutas',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './wikiloc-rutas.html',
   styleUrl: './wikiloc-rutas.css'
 })
 export class WikilocRutas {
-      activo: 'info' | 'mapa' | null = null;
+  
+  activo: 'info' | 'mapa' | null = null;
 
   toggle(estacion: 'info' | 'mapa') {
     this.activo = this.activo === estacion ? null : estacion;
   }
 }
+
+
