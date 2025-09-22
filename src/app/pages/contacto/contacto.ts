82f14e8f89eb93727880da8179a204132f31fc81
Laura Moreno <chipifsdev@gmail.com>
2025-09-22 13:36:09 +0200

    empezado el formulario para pedir las prendas alfareras, continuar por aquí, select color desaparece con opciones que no sean camiseta tecnica

--- src/app/pages/contacto/contacto.ts
+++ src/app/pages/contacto/contacto.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
-  imports: [ ],
+  imports: [ReactiveFormsModule ],
   templateUrl: './contacto.html',
   styleUrl: './contacto.css'
 })
 export class Contacto {
 
+  pedidoForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.pedidoForm = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      telefono: ['', [Validators.required, Validators.pattern('^[0-9]{9}$')]],
+      prenda: ['', Validators.required],
+      color: ['', Validators.required],
+      talla: ['', Validators.required],
+      observaciones: ['']
+    });
+    
+      this.pedidoForm.get('prenda')?.valueChanges.subscribe((value) => {
+          const colorControl = this.pedidoForm.get('color');
+          if (value === 'camiseta-algodon' || value === 'camiseta-tecnica') {
+            colorControl?.enable(); // activa
+          } else {
+            colorControl?.disable(); // desactiva
+            colorControl?.reset();   // limpia el valor
+          }
+        });
+      }
+
+  onSubmit() {
+    if (this.pedidoForm.valid) {
+      console.log('Pedido enviado:', this.pedidoForm.value);
+      alert('Pedido enviado con éxito 🎉');
+      this.pedidoForm.reset();
+    } else {
+      alert('Por favor, revisa los campos del formulario.');
+    }
+  }
 }
+
