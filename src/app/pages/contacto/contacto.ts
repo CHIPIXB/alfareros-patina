f1bbba5bc4936b4d16a360d3e44e7f5b169a5a46
Laura Moreno <chipifsdev@gmail.com>
2026-01-07 17:32:05 +0100

    añadido select para las tallas del formulario

--- src/app/pages/contacto/contacto.ts
+++ src/app/pages/contacto/contacto.ts
@@ -18,21 +18,34 @@ export class Contacto {
       nombre: ['', [Validators.required, Validators.minLength(3)]],
       telefono: ['', [Validators.required, Validators.pattern('^[0-9]{9}$')]],
       prenda: ['', Validators.required],
-      color: ['', Validators.required],
+      color: [''],
       talla: ['', Validators.required],
       observaciones: ['']
     });
     
-      this.pedidoForm.get('prenda')?.valueChanges.subscribe((value) => {
-          const colorControl = this.pedidoForm.get('color');
-          if (value === 'camiseta-algodon' || value === 'camiseta-tecnica') {
-            colorControl?.enable(); // activa
-          } else {
-            colorControl?.disable(); // desactiva
-            colorControl?.reset();   // limpia el valor
-          }
-        });
+    this.pedidoForm.get('prenda')?.valueChanges.subscribe((value) => {
+      const colorControl = this.pedidoForm.get('color');
+      const tallaControl = this.pedidoForm.get('talla');
+
+      // 🎨 Color solo si camiseta técnica
+      if (value === 'camiseta-tecnica') {
+        colorControl?.setValidators([Validators.required]);
+      } else {
+        colorControl?.clearValidators();
+        colorControl?.reset();
       }
+      colorControl?.updateValueAndValidity();
+
+      // 👕 Talla en todas menos mochila
+      if (value && value !== 'mochila') {
+        tallaControl?.setValidators([Validators.required]);
+      } else {
+        tallaControl?.clearValidators();
+        tallaControl?.reset();
+      }
+      tallaControl?.updateValueAndValidity();
+    });
+}
 
   onSubmit() {
     if (this.pedidoForm.valid) {
